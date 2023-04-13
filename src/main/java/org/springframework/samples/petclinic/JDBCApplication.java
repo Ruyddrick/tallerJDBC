a4d7096350941e83bbdd8e2b0b26fb6e5523a855
Rodrigo <2014roag@gmail.com>
2023-04-13 13:22:20 +0200

    arreglos en la conexión

--- src/main/java/org/springframework/samples/petclinic/JDBCApplication.java
+++ src/main/java/org/springframework/samples/petclinic/JDBCApplication.java
@@ -4,6 +4,7 @@ package org.springframework.samples.petclinic;
 import java.sql.Connection;
 
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -31,12 +32,45 @@ public class JDBCApplication {
 		
 		
 		try {
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/petclinic","root", "root");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/petclinic?serverTimezone=UTC&useSSL=false","desarrollo", "123");
 			if (connection != null)
 				System.out.println("Conexión establecida");
 			
-		
-			
+// Primer ejercicio del taller
+/*   Borrar para comentar ejercicio =====> */
+		System.out.println("\nPrimer ejercicio de Bootcamp Java NTT Data\n  ******************* ******************\n");
+		Statement query = null;
+		ResultSet result = null;
+		try {
+			query = connection.createStatement();
+			String SQL = "SELECT * FROM petclinic.owners";
+			result = query.executeQuery(SQL);	
+		}
+		catch (SQLException e) {
+		   System.out.print("Error al ejecutar la consulta "+ e);
+		}
+		finally {
+		   	   
+		   if(result != null) {
+			   String nombre;
+			   String apellido;
+			   String ciudad;
+			   String direccion;
+			   String telefono;
+			   while (result.next()) {
+				  nombre = result.getString("first_name");
+				  apellido = result.getString("last_name");
+				  ciudad = result.getString("city");
+				  direccion = result.getString("address");
+				  telefono = result.getString("telephone");
+				  System.out.println(nombre+" "+apellido+" => "+" Teléfono: "+telefono+" // "+ciudad+" - "+direccion);
+			   }
+			   result.close();
+		   }
+		   
+		   query.close();
+		}
+//	Final del primer ejercicio del taller */
 			
 			
 			
